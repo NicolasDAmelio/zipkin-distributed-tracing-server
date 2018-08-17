2be4a2ca54b615c3ca2ed73002356201d8362b9a
Nicolas D'Amelio <ndamelio@capitaria.com>
2018-08-17 16:31:19 -0300

    finishhh

--- src/main/java/com/ndamelio/learning/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
+++ src/main/java/com/ndamelio/learning/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -2,7 +2,9 @@ package com.ndamelio.learning.zipkindistributedtracingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinDistributedTracingServerApplication {
 
